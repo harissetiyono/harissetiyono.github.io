ddc01d3e9af970fbc93cfe44f939e96fbdaf4f94
Haris Setiyono <harissetiyono@gmail.com>
2024-03-02 08:21:38 +0700

    Refactor Image component to use JSX syntax

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,7 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/harissetiyono.github.io${src}`} {...rest} />
+const Image = ({ src, ...rest }: ImageProps) => (
+  <NextImage src={`/harissetiyono.github.io${src}`} {...rest} />
+)
 
 export default Image
