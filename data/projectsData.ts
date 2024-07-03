c5c18109dcf5f88fcbb2deeaf2ac19d1540d72b7
Haris Setiyono <harissetiyono@gmail.com>
2024-07-03 21:38:20 +0700

    fix: wrong link others stuff projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,8 +1,8 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
@@ -10,19 +10,19 @@ const projectsData: Project[] = [
     title: 'BisaCourse - Online Course UI App',
     description: ``,
     imgSrc: '/static/images/ui_design/BisaCook.png',
-    href: 'https://dribbble.com/shots/24453726-BisaCourse-Online-Course-UI-App',
+    href: 'https://dribbble.com/shots/24453700-BisaCook-Receipt-UI-App',
   },
   {
     title: 'BisaFR - Face Recognition UI App',
     description: ``,
     imgSrc: '/static/images/ui_design/BisaFaceRecognition.png',
-    href: 'https://dribbble.com/shots/24453726-BisaCourse-Online-Course-UI-App',
+    href: 'https://dribbble.com/shots/24453712-BisaFR-Face-Recognition-UI-App',
   },
   {
     title: 'BisaCourse - Online Course UI App',
     description: ``,
     imgSrc: '/static/images/ui_design/BisaCourse.png',
-    href: 'https://dribbble.com/shots/24453726-BisaCourse-Online-Course-UI-App',
+    href: 'https://dribbble.com/shots/24453712-BisaFR-Face-Recognition-UI-App',
   },
   {
     title: 'Mobile Developer Flutter Guidelines',
