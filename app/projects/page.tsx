fdc6b800121ef99a6448d95aa58dca55d1757ab0
Haris Setiyono <harissetiyono@gmail.com>
2024-07-04 07:54:09 +0700

    fix: remove unused text

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -31,7 +31,6 @@ export default function Projects() {
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400"></p>
         </div>
         <div className="container py-12">
-          Others
           <div className="-m-4 flex flex-wrap">
             {project.map((d) => (
               <Card
