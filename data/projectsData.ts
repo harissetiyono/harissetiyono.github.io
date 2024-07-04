d5651efce98106e948a786bf1339ab3297a71747
Haris Setiyono <harissetiyono@gmail.com>
2024-07-04 07:49:16 +0700

    update: change format image to webp to better performance

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,25 +9,25 @@ const projectsData: Project[] = [
   {
     title: 'BisaCourse - Online Course UI App',
     description: ``,
-    imgSrc: '/static/images/ui_design/BisaCook.png',
+    imgSrc: '/static/images/ui_design/BisaCook.webp',
     href: 'https://dribbble.com/shots/24453700-BisaCook-Receipt-UI-App',
   },
   {
     title: 'BisaFR - Face Recognition UI App',
     description: ``,
-    imgSrc: '/static/images/ui_design/BisaFaceRecognition.png',
+    imgSrc: '/static/images/ui_design/BisaFaceRecognition.webp',
     href: 'https://dribbble.com/shots/24453712-BisaFR-Face-Recognition-UI-App',
   },
   {
     title: 'BisaCourse - Online Course UI App',
     description: ``,
-    imgSrc: '/static/images/ui_design/BisaCourse.png',
+    imgSrc: '/static/images/ui_design/BisaCourse.webp',
     href: 'https://dribbble.com/shots/24453726-BisaCourse-Online-Course-UI-App',
   },
   {
     title: 'Mobile Developer Flutter Guidelines',
     description: ``,
-    imgSrc: '/static/images/others/flutter_guidelines_cover.jpeg',
+    imgSrc: '/static/images/others/flutter_guidelines_cover.webp',
     href: 'https://harissetiyono.id/mobile-developer-flutter-guidelines/',
   },
 ]
